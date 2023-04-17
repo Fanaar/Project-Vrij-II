004a069c49560f24d9765cccb177db37b2463237
Fanaar Rashid <fanaarrashid@gmail.com>
2023-04-17 13:33:40 +0200

    Aiming animations

--- Project Vrij II/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Project Vrij II/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool isAiming;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,6 +44,11 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnAiming(InputValue value)
+        {
+			isAiming = value.isPressed;
+        }
 #endif
 
 
